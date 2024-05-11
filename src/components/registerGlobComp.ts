a13b8d056dc0c3821902007e2b55fd493e85f785
real-ju <2201181566@qq.com>
2024-05-11 17:39:27 +0800

    add antd

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -1,3 +1,13 @@
 import type { App } from 'vue';
 
-export function registerGlobComp(app: App) {}
+import { Input, InputNumber, Select, Table, Form, FormItem } from 'ant-design-vue/es';
+
+export function registerGlobComp(app: App) {
+  // antd components register
+  app.component(Input.name, Input);
+  app.component(InputNumber.name, InputNumber);
+  app.component(Select.name, Select);
+  app.component(Table.name, Table);
+  app.component(Form.name, Form);
+  app.component(FormItem.name, FormItem);
+}
